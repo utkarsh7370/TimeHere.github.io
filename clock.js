495bef79b8b963a6ce4d74a378c76e789950dd39
Utkarsh Singh <83408377+utkarsh7370@users.noreply.github.com>
2022-02-17 17:31:40 +0530

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,16 @@
+const clocksound = new Audio("clock-ticking-2.wav");
+setInterval(() => {
+    d = new Date();
+    h = d.getHours();
+    m = d.getMinutes();
+    s = d.getSeconds();
+
+    hr = 30 * h + m / 2;
+    mr = 6 * m;
+    sr = 6 * s;
+
+    hour.style.transform = `rotate(${hr}deg)`;
+    minute.style.transform = `rotate(${mr}deg)`;
+    second.style.transform = `rotate(${sr}deg)`;
+    clocksound.play();
+}, 1000);
